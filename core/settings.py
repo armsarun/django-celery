43176c456197f4940b40ce489a4bc026c78ef355
arunkumar <arunkumarp93@yahoo.com>
2020-05-03 20:55:14 +0530

    settings udpated

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'fisdom-demo.herokuapp.com']
 
@@ -75,32 +75,17 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 if DEBUG:
-    # DATABASES = {
-    #     'default': {
-    #         'ENGINE': 'django.db.backends.postgresql',
-    #         'NAME': config('DATABASE_NAME'),
-    #         'USER': config('DATABASE_USER'),
-    #         'PASSWORD': config('DATABASE_PASSWORD'),
-    #         'HOST': config('DATABASE_HOST'),
-    #         'PORT': config('DATABASE_PORT'),
-    #     }
-    # }
-    import dj_database_url
-
-    # set server database configuration here
-
     DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql_psycopg2",
-            'NAME': 'fisdomapp',
-            'USER': 'name',
-            'PASSWORD': '',
-            'HOST': 'localhost',
-            'PORT': '',
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config('DATABASE_NAME'),
+            'USER': config('DATABASE_USER'),
+            'PASSWORD': config('DATABASE_PASSWORD'),
+            'HOST': config('DATABASE_HOST'),
+            'PORT': config('DATABASE_PORT'),
         }
     }
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(db_from_env)
+
 else:
 
     import dj_database_url
@@ -164,9 +149,14 @@ STATICFILES_DIRS = os.path.join(BASE_DIR, "core/static"),
 
 # Celery settings
 if DEBUG:
-    # CELERY_BROKER_URL = config('CELERY_BROKER_URL')
-    # CELERY_RESULT_BACKEND = config('CELERY_RESULT_BACKEND')
+    CELERY_BROKER_URL = config('CELERY_BROKER_URL')
+    CELERY_RESULT_BACKEND = config('CELERY_RESULT_BACKEND')
     CELERY_ACCEPT_CONTENT = ['application/json']
     CELERY_TASK_SERIALIZER = 'json'
     CELERY_RESULT_SERIALIZER = 'json'
     CELERY_TIMEZONE = 'Asia/Kolkata'
+
+
+if not DEBUG:
+    MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
