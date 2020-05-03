1493ed7030622956e3313cd3a824390aa653a341
arunkumar <arunkumarp93@yahoo.com>
2020-05-03 21:07:01 +0530

    updated static

--- core/settings.py
+++ core/settings.py
@@ -75,16 +75,32 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 if DEBUG:
+    # DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.postgresql',
+    #         'NAME': config('DATABASE_NAME'),
+    #         'USER': config('DATABASE_USER'),
+    #         'PASSWORD': config('DATABASE_PASSWORD'),
+    #         'HOST': config('DATABASE_HOST'),
+    #         'PORT': config('DATABASE_PORT'),
+    #     }
+    # }
+    import dj_database_url
+
+    # set server database configuration here
+
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': config('DATABASE_NAME'),
-            'USER': config('DATABASE_USER'),
-            'PASSWORD': config('DATABASE_PASSWORD'),
-            'HOST': config('DATABASE_HOST'),
-            'PORT': config('DATABASE_PORT'),
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            'NAME': 'fisdomapp',
+            'USER': 'name',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': '',
         }
     }
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
 
 else:
 
