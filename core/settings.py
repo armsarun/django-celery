4efa3ff4ede5d3fdec0d49a126760a857099ea84
arunkumar <arunkumarp93@yahoo.com>
2020-05-28 18:04:19 +0530

    updated settings

--- core/settings.py
+++ core/settings.py
@@ -78,16 +78,18 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': config('DATABASE_NAME'),
-            'USER': config('DATABASE_USER'),
-            'PASSWORD': config('DATABASE_PASSWORD'),
-            'HOST': config('DATABASE_HOST'),
-            'PORT': config('DATABASE_PORT'),
-        }
-    }
+
+    pass
+    # DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.postgresql',
+    #         'NAME': config('DATABASE_NAME'),
+    #         'USER': config('DATABASE_USER'),
+    #         'PASSWORD': config('DATABASE_PASSWORD'),
+    #         'HOST': config('DATABASE_HOST'),
+    #         'PORT': config('DATABASE_PORT'),
+    #     }
+    # }
 
 else:
 
